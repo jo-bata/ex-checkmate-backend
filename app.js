2c9102f05e98322e3a715429b8ad079d059d92d1
jo-bata <jobata@skuniv.ac.kr>
2018-08-03 22:27:16 +0900

    Init App

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 
 const config = require('./config/config');
 const auth = require('./routes/auth');
+const user = require('./routes/user');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/auth/', auth);
+app.use('/user/', user);
 
 module.exports = app;
---
+++ routes/user.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+const conn = require('../config/mysql');
+
+router.get('/:userId/work', function(req, res) {
+
+});
+
+router.post('/:userId/work', function(req, res) {
+
+});
+
+router.get('/:userId/work/:workId', function(req, res) {
+
+});
+
+router.put('/:userId/work/:workId', function(req, res) {
+
+});
+
+router.delete('/:userId/work/:workId', function(req, res) {
+
+});
+
+module.exports = router;
