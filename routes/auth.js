32b541ff92d03be424e1f65d7d4240d1bb3e2217
jo-bata <jobata@skuniv.ac.kr>
2018-08-07 17:01:06 +0900

    feat: Add login section

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,36 @@
 const router = require('express').Router();
 
 const conn = require('../config/mysql');
+const config = require('../config/config');
 
-router.get('/login', function(req, res) {
-
+router.post('/login', function(req, res) {
+  const user = {
+    "id": req.body.id,
+    "nickname": req.body.nickname,
+    "email": req.body.email
+  };
+  const sql = 'SELECT * FROM users WHERE id=?';
+  conn.query(sql, [user.id], function(err, results) {
+    if(err) {
+      console.log(err);
+      req.status(500).json(config.status.sc500);
+    } else if(results.length === 0) {
+      console.log('New user access !');
+      const sql = 'INSERT INTO users SET ?';
+      conn.query(sql, user, function(err, results) {
+        if(err) {
+          console.log(err);
+          req.status(500).json(config.status.sc500);
+        } else {
+          console.log('Add new user !');
+          req.status(200).json(config.status.sc200);
+        }
+      });
+    } else {
+      console.log('Existing user access !');
+      req.status(200).json(config.status.sc200);
+    }
+  });
 });
 
 module.exports = router;
