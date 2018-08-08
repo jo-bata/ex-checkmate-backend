8a5ce5a3d4663ae337bf66ae96a4276ed1bb289e
jo-bata <jobata@skuniv.ac.kr>
2018-08-08 19:00:28 +0900

    feat: Add a single workplace API

--- routes/user.js
+++ routes/user.js
@@ -1,44 +1,71 @@
 const router = require('express').Router();
 
 const conn = require('../config/mysql');
+const config = require('../config/config');
+const func = require('../config/function');
 
-router.get('/', function(req, res) {
-
-});
-
-router.post('/', function(req, res) {
+router.get('/:userId/work', function(req, res) {
 
 });
 
-router.get('/:workId', function(req, res) {
+router.post('/:userId/work', function(req, res) {
+  func.workIdGenerator(req, req.params.userId, function(workId) {
+    const work = {
+      "id": workId,
+      "user_id": req.params.userId,
+      "name": req.body.name,
+      "address": req.body.address,
+      "latitude": req.body.latitude,
+      "longitude": req.body.longitude,
+      "hourly_wage": req.body.hourly_wage,
+      "probation": req.body.probation,
+      "recess": req.body.recess,
+      "recess_state": req.body.recess_state,
+      "pay_day": req.body.pay_day,
+      "tax": req.body.tax
+    };
+    const sql = 'INSERT INTO works SET ?';
+    conn.query(sql, work, function(err, results) {
+      if(err) {
+        console.log(err);
+        res.status(500).json(config.status.sc500);
+      } else {
+        console.log('Add new work !');
+        res.status(200).json(config.status.sc200);
+      }
+    });
+  });
+});
+
+router.get('/:userId/work/:workId', function(req, res) {
 
 });
 
-router.put('/:workId', function(req, res) {
+router.put('/:userId/work/:workId', function(req, res) {
 
 });
 
-router.delete('/:workId', function(req, res) {
+router.delete('/:userId/work/:workId', function(req, res) {
 
 });
 
-router.get('/:workId/main', function(req, res) {
+router.get('/:userId/work/:workId/main', function(req, res) {
 
 });
 
-router.post('/:workId/main', function(req, res) {
+router.post('/:userId/work/:workId/main', function(req, res) {
 
 });
 
-router.get('/:workId/main/detail', function(req, res) {
+router.get('/:userId/work/:workId/main/detail', function(req, res) {
 
 });
 
-router.get('/:workId/main/calendar/:year/:month', function(req, res) {
+router.get('/:userId/work/:workId/main/calendar/:year/:month', function(req, res) {
 
 });
 
-router.get('/:workId/main/calendar/:year/:month/day', function(req, res) {
+router.get('/:userId/work/:workId/main/calendar/:year/:month/day', function(req, res) {
 
 });
 
